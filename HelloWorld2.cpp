cccce26555ac0787a5811734cf3fc677b7925965
Antokl <liantokha@mail.ru>
2024-07-31 13:38:39 +0300

    Изменили комментарии и добавили новые строки.

--- HelloWorld2.cpp
+++ HelloWorld2.cpp
@@ -1,24 +1,34 @@
 #include <iostream>
 
-// This function prints
+// Вызывает функцию std::cout
 void print()
 {
-    // This statment prints
+    // Потому что хотим использовать стандартную библиотеку для печати в консоль и
+    // не использовать какие либо другие библиотеки.
     std::cout << "Hello Skillbox\n"; // This function prints
 }
 
 /*
-*1
-*2
-*3
-*4
+* Присваиваем значение переменным и печатаем hello world!
 */
 int main()
 {
-
     int x = 100;
+    3 + 7;
     int y = x + 100;
-    int mult = x * y;
 
-    std::cout << "Hello World!";
-}
+    int b = 0;
+    b = b + 2;
+
+    int test;
+    test = 100500;
+
+    int test2 = 1005001;
+
+
+
+    int mult = x * y;
+    int random = 1002;
+
+    std::cout << random;
+} 

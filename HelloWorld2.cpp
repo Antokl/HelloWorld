a9cbbc9acd7c6f61e685c3edd5d57fcb8176541a
Antokl <liantokha@mail.ru>
2024-07-31 13:01:31 +0300

    Добавлена новая строка Hello Skillbox

--- HelloWorld2.cpp
+++ HelloWorld2.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
 
+void print()
+{
+    std::cout << "Hello Skillbox\n";
+}
 int main()
 {
+
     int x = 100;
     int y = x + 100;
+    int mult = x * y;
+
     std::cout << "Hello World!";
 
-    int mult = x* y;
 }
